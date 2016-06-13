c3b557210561d75efe10b935fea3e97b57e53421
k014c1141 <k014c1141@it-neec.jp>
2016-06-13 09:52:50 +0900

    second commit

--- db.sh
+++ db.sh
@@ -5,5 +5,5 @@ mysql -uroot  -e "drop database board;"
 
 mysql -uroot  -e "create database board character set UTF8;"
 
-mysql -uroot  board -e "create table log (id MEDIUMINT NOT NULL AUTO_INCREMENT  , message varchar(256) ,sdate varchar(256) PRIMARY KEY(id));"
+mysql -uroot  board -e "create table log (id MEDIUMINT NOT NULL AUTO_INCREMENT  , username varchar(256) ,message varchar(256) ,sdate varchar(256) PRIMARY KEY(id));"
 
